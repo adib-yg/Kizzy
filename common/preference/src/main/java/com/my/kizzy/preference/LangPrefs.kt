817c81f10fa151909730b2ae3cbb3248b5915aa9
Adib <microsoftuser3030@gmail.com>
2024-02-07 07:51:55 +0330

    Update LangPrefs.kt - Use enum

--- common/preference/src/main/java/com/my/kizzy/preference/LangPrefs.kt
+++ common/preference/src/main/java/com/my/kizzy/preference/LangPrefs.kt
@@ -41,21 +41,21 @@ enum class LANGUAGES
 
 val languages: Map<Int, String> =
     mapOf(
-        Pair(ENGLISH, "en"),
-        Pair(TURKISH, "tr"),
-        Pair(DUTCH,"nl"),
-        Pair(RUSSIAN, "ru"),
-        Pair(POLISH, "pl"),
-        Pair(PORTUGUESE, "pt"),
-        Pair(INDONESIAN, "in"),
-        Pair(SIMPLIFIED_CHINESE, "zh"),
-        Pair(BURMESE, "mm"),
-        Pair(VIETNAMESE, "vi"),
-        Pair(ITALIAN, "it"),
-        Pair(FILIPINO, "fil"),
-        Pair(FRENCH, "fr"),
-        Pair(CROATIAN,"hr"),
-        Pair(FARSI, "fa")
+        Pair(LANGUAGES.ENGLISH.ordinal, "en"),
+        Pair(LANGUAGES.TURKISH.ordinal, "tr"),
+        Pair(LANGUAGES.DUTCH.ordinal,"nl"),
+        Pair(LANGUAGES.RUSSIAN.ordinal, "ru"),
+        Pair(LANGUAGES.POLISH.ordinal, "pl"),
+        Pair(LANGUAGES.PORTUGUESE.ordinal, "pt"),
+        Pair(LANGUAGES.INDONESIAN.ordinal, "in"),
+        Pair(LANGUAGES.SIMPLIFIED_CHINESE.ordinal, "zh"),
+        Pair(LANGUAGES.BURMESE.ordinal, "mm"),
+        Pair(LANGUAGES.VIETNAMESE.ordinal, "vi"),
+        Pair(LANGUAGES.ITALIAN.ordinal, "it"),
+        Pair(LANGUAGES.FILIPINO.ordinal, "fil"),
+        Pair(LANGUAGES.FRENCH.ordinal, "fr"),
+        Pair(LANGUAGES.CROATIAN.ordinal,"hr"),
+        Pair(LANGUAGES.FARSI.ordinal, "fa")
     )
 
 fun getLanguageConfig(languageNumber: Int = Prefs[Prefs.LANGUAGE]): String {
@@ -66,7 +66,7 @@ private fun getLanguageNumberByCode(languageCode: String): Int {
     languages.entries.forEach {
         if (it.value == languageCode) return it.key
     }
-    return SYSTEM_DEFAULT
+    return LANGUAGES.SYSTEM_DEFAULT.ordinal
 }
 
 fun getLanguageNumber(): Int {
@@ -74,28 +74,28 @@ fun getLanguageNumber(): Int {
         getLanguageNumberByCode(
             LocaleListCompat.getAdjustedDefault()[0]?.toLanguageTag().toString()
         )
-    else Prefs[Prefs.LANGUAGE, SYSTEM_DEFAULT]
+    else Prefs[Prefs.LANGUAGE, LANGUAGES.SYSTEM_DEFAULT.ordinal]
 }
 
 @Composable
 fun getLanguageDesc(language: Int = getLanguageNumber()): String {
     return stringResource(
         when (language) {
-            SIMPLIFIED_CHINESE -> R.string.locale_zh
-            ENGLISH -> R.string.locale_en
-            TURKISH -> R.string.locale_tr
-            RUSSIAN -> R.string.locale_ru
-            INDONESIAN -> R.string.locale_in
-            DUTCH -> R.string.locale_nl
-            POLISH -> R.string.locale_pl
-            PORTUGUESE -> R.string.locale_pt
-            BURMESE -> R.string.locale_mm
-            VIETNAMESE -> R.string.locale_vi
-            ITALIAN -> R.string.locale_it
-            FILIPINO -> R.string.locale_fil
-            FRENCH -> R.string.locale_fr
-            CROATIAN -> R.string.locale_hr
-            FARSI -> R.string.locale_fa
+            LANGUAGES.SIMPLIFIED_CHINESE.ordinal -> R.string.locale_zh
+            LANGUAGES.ENGLISH.ordinal -> R.string.locale_en
+            LANGUAGES.TURKISH.ordinal -> R.string.locale_tr
+            LANGUAGES.RUSSIAN.ordinal -> R.string.locale_ru
+            LANGUAGES.INDONESIAN.ordinal -> R.string.locale_in
+            LANGUAGES.DUTCH.ordinal -> R.string.locale_nl
+            LANGUAGES.POLISH.ordinal -> R.string.locale_pl
+            LANGUAGES.PORTUGUESE.ordinal -> R.string.locale_pt
+            LANGUAGES.BURMESE.ordinal -> R.string.locale_mm
+            LANGUAGES.VIETNAMESE.ordinal -> R.string.locale_vi
+            LANGUAGES.ITALIAN.ordinal -> R.string.locale_it
+            LANGUAGES.FILIPINO.ordinal -> R.string.locale_fil
+            LANGUAGES.FRENCH.ordinal -> R.string.locale_fr
+            LANGUAGES.CROATIAN.ordinal -> R.string.locale_hr
+            LANGUAGES.FARSI.ordinal -> R.string.locale_fa
             else -> R.string.follow_system
         }
     )
